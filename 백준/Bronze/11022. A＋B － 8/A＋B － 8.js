65547175193fb42eaea83e98308976d86be01614
최서연 <143925634+seoyeon5117@users.noreply.github.com>
2025-02-10 18:14:02 +0900

    [Bronze V] Title: A+B - 8, Time: 104 ms, Memory: 9544 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,13 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const num = parseInt(input[0]);
+
+for (let i = 1; i < num + 1; i++) {
+  const [a, b] = input[i].split(" ").map((el) => parseInt(el));
+  const result = a + b;
+  console.log("Case #" + i + ": " + a + ' + ' + b + ' = ' + result);
+}
