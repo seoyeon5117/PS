1b94787aeb9a7d6061e7d282407c9db41d824e59
최서연 <143925634+seoyeon5117@users.noreply.github.com>
2025-05-03 08:31:42 +0900

    [Bronze II] Title: 블랙잭, Time: 164 ms, Memory: 9944 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -1,22 +1,33 @@
-const input = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .split("\n")
-    .map((el) => el.split(" ").map(Number));
-const n = input[0][0];
-const m = input[0][1];
-let max = 3;
-let sum = 0;
-for(let i = 0 ; i < n ; i++) {
-    for (let j = 0; j < n; j++) {
-        if (i !== j) {
-            for (let k = 0; k < n; k++)
-                if(i !== k && j !== k) {
-                    sum = input[1][i] + input[1][j] + input[1][k];
-                    if (max < sum && sum <= m)
-                        max = sum;
-                }
-        }
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", (line) => {
+  input.push(line);
+  if (line.length === 2) {
+    rl.close();
+  }
+});
+
+rl.on("close", () => {
+  const [N, M] = input[0].split(" ").map(Number);
+  const cards = input[1].split(" ").map(Number);
+  let result = 0;
+
+  for (let i = 0; i < N; i++) {
+    for (let k = 1; k < N; k++) {
+      for (let l = 2; l < N; l++) {
+        let sum = cards[i] + cards[k] + cards[l];
+        if (sum <= M && result < sum && i !== k && i !== l && k !== l)
+          result = sum;
+      }
     }
-}
-console.log(max);
+  }
+
+  console.log(result);
+});
