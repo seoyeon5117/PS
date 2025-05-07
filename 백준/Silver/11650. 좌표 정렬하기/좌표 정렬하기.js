ea6436e670f371d251ffa418d36f1fd50936623a
최서연 <143925634+seoyeon5117@users.noreply.github.com>
2025-05-07 17:22:27 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 548 ms, Memory: 55476 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,30 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", (line) => {
+  input.push(line);
+  if (input.length === Number(input[0]) + 1) {
+    rl.close();
+  }
+});
+
+rl.on("close", () => {
+  const N = Number(input[0]);
+  const dot = [];
+  for (let i = 1; i <= N; i++) {
+    dot.push(input[i].split(" ").map(Number));
+  }
+  dot.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  });
+  console.log(dot.map((v) => v.join(" ")).join("\n"));
+});
