9690b356451cec30576e0a4d1022547e3ac5da49
SeoYeon Choi <143925634+seoyeon5117@users.noreply.github.com>
2026-02-24 03:04:23 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 936 ms, Memory: 105248 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -12,27 +12,32 @@ rl.on("line", (line) => {
 });
 
 rl.on("close", () => {
-  const [N, M] = input[0].split(" ").map(Number);
-  const graph = Array.from({ length: N + 1 }, () => new Array());
-  const visited = new Array(N + 1).fill(false);
+  const [N, M] = input[0].split(" ").map((c) => Number(c));
+  const graph = Array.from({ length: N + 1 }, () => []);
 
   for (let i = 1; i <= M; i++) {
-    const [a, b] = input[i].split(" ").map(Number);
-    graph[a].push(b);
-    graph[b].push(a);
-  }
-
-  function dfs(i) {
-    visited[i] = true;
-    for (const node of graph[i]) {
-      if (!visited[node]) dfs(node);
-    }
+    const [n, m] = input[i].split(" ").map((c) => Number(c));
+    graph[n].push(m);
+    graph[m].push(n);
   }
 
   let count = 0;
+
+  const visited = new Set();
+  function dfs(node) {
+    visited.add(node);
+
+    for (const next of graph[node]) {
+      if (!visited.has(next)) {
+        dfs(next);
+      }
+    }
+  }
+
   for (let i = 1; i <= N; i++) {
-    if (!visited[i]) {
-      dfs(i);
+    const size = visited.size;
+    dfs(i);
+    if (size < visited.size) {
       count++;
     }
   }
