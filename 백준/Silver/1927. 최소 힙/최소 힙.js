f099aba083388edf582f665b0174a57a340a0a63
SeoYeon Choi <143925634+seoyeon5117@users.noreply.github.com>
2026-03-06 09:48:24 +0900

    [Silver II] Title: 최소 힙, Time: 292 ms, Memory: 29472 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.js
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -1,4 +1,3 @@
-const { join } = require("path");
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -12,25 +11,18 @@ rl.on("line", (line) => {
   input.push(line);
 });
 
-// 첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다.
-// 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다.
-// 만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고,
-// x가 0이라면 배열에서 가장 작은 값을 출력하고 그 값을 배열에서 제거하는 경우이다.
-// x는 231보다 작은 자연수 또는 0이고, 음의 정수는 입력으로 주어지지 않는다.
-
 rl.on("close", () => {
   const N = +input[0];
-  const arr = input.slice(1).map(Number);
   const heap = [];
 
   function insert(x) {
     heap.push(x);
     let i = heap.length - 1;
     while (i > 0) {
-      const parentIdx = Math.floor((i - 1) / 2);
-      if (heap[parentIdx] > heap[i]) {
-        [heap[parentIdx], heap[i]] = [heap[i], heap[parentIdx]];
-        i = parentIdx;
+      const parent = Math.floor((i - 1) / 2);
+      if (heap[parent] > heap[i]) {
+        [heap[i], heap[parent]] = [heap[parent], heap[i]];
+        i = parent;
       } else {
         break;
       }
@@ -41,6 +33,7 @@ rl.on("close", () => {
     if (heap.length === 0) return 0;
     const root = heap[0];
     const lastNode = heap.pop();
+
     if (heap.length > 0) {
       heap[0] = lastNode;
       let i = 0;
@@ -61,10 +54,12 @@ rl.on("close", () => {
 
   const result = [];
 
-  for (const a of arr) {
-    if (a === 0) result.push(remove());
-    else insert(a);
+  for (let i = 1; i <= N; i++) {
+    if (+input[i] === 0) {
+      result.push(remove());
+    } else {
+      insert(+input[i]);
+    }
   }
-
   console.log(result.join("\n"));
 });
