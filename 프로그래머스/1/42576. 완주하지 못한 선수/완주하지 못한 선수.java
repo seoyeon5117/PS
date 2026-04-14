86450a645b331a979ba5d2820ae5820790bb71cd
SeoYeon Choi <143925634+seoyeon5117@users.noreply.github.com>
2026-04-14 16:17:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.22 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,17 +1,23 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        HashMap<String, Integer> map = new HashMap<>();
         
-        for(int i = 0 ; i < completion.length ; i++) {
-            if(!participant[i].equals(completion[i])) {
-                return participant[i];
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for(String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) >= 1) {
+                return key;
             }
         }
         
-        return participant[participant.length - 1];
+        return "";
     }
 }
