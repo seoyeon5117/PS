aa0ae4f70c2759fb08e9ca9fe36d57062e7c782c
SeoYeon Choi <143925634+seoyeon5117@users.noreply.github.com>
2026-05-04 15:13:09 +0900

    [level 2] Title: 전화번호 목록, Time: 296.18 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,16 +1,4 @@
 function solution(phone_book) {
-    const map = new Map();
-    
-    for(let i = 0 ; i < phone_book.length ; i++) {
-        map.set(phone_book[i], true);
-    }
-    
-    for(const num of phone_book) {
-        for(let j = 1 ; j < num.length ; j++) {
-            if(map.has(num.slice(0, j))) {
-                return false;
-            }
-        }
-    }
-    return true;
+    var answer = true;
+    return answer;
 }
