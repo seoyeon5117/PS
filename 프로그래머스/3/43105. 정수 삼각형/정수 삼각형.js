e75bcf05aa04ea31b3c451e2170367dce2ba7d9c
SeoYeon Choi <143925634+seoyeon5117@users.noreply.github.com>
2026-06-25 12:31:40 +0900

    [level 3] Title: 정수 삼각형, Time: 8.29 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,25 @@
+function solution(triangle) {
+    let answer = 0;
+    const height = triangle.length;
+    const cnt = (height + 1) * height / 2;
+    const dp = Array.from({length : height}, () => new Array(height));
+    dp[0][0] = triangle[0][0];
+    
+    for(let i = 1 ; i < height ; i++) {
+        for(let j = 0 ; j < i + 1 ; j++) {
+            if(j === 0) {
+                dp[i][j] = dp[i-1][j] + triangle[i][j];
+            } else if(j === i) {
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+            } else {
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+            }
+        }
+    }
+    
+    for(let k = 0 ; k < height ; k++) {
+        answer = Math.max(answer, dp[height - 1][k]);
+    }
+    
+    return answer;
+}
